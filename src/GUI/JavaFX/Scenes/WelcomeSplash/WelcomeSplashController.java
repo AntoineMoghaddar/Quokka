39da6412a05305c1340c3aa125632ec4b74d956b
Moose <antoine_moghaddar@hotmail.com>
2017-04-12 12:24:59 +0200

    SPLASH FINISHED

--- src/GUI/JavaFX/Scenes/WelcomeSplash/WelcomeSplashController.java
+++ src/GUI/JavaFX/Scenes/WelcomeSplash/WelcomeSplashController.java
@@ -1,12 +1,22 @@
 package GUI.JavaFX.Scenes.WelcomeSplash;
 
 import Design.Logger;
+import javafx.beans.binding.Bindings;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ToggleButton;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.*;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
 
-import java.io.File;
+import java.awt.*;
+import javafx.geometry.Insets;
 import java.net.URL;
 import java.util.ResourceBundle;
 import java.util.Timer;
@@ -18,13 +28,22 @@ import java.util.TimerTask;
 public class WelcomeSplashController implements Initializable {
 
     @FXML
-    private ImageView iv;
+    private Text textWelcomeSplash;
+
+    @FXML
+    private VBox mainWelcome;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        File file = new File("/img.png");
-        Image img = new Image(file.toURI().toString());
-        iv.setImage(img);
+        ToggleButton toggle = new ToggleButton("Toggle color");
+
+        textWelcomeSplash.setText("Welcome to Quokka");
+
+        mainWelcome.backgroundProperty().bind(Bindings.when(toggle.selectedProperty())
+                .then(new Background(new BackgroundFill(Color.CORNFLOWERBLUE, CornerRadii.EMPTY, Insets.EMPTY)))
+                .otherwise(new Background(new BackgroundFill(Color.LIGHTBLUE , CornerRadii.EMPTY, Insets.EMPTY))));
+
+//        mainWelcome.setBackground(new Background(new BackgroundFill(new Color(215,209,209, 100))));
         Timer timer = new Timer();
         timer.schedule(new TimerTask() {
             @Override
